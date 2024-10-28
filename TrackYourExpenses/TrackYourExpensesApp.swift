340fb013d92fd83abf3107a8e8ea7797380b89aa
Abdul <ahusaynn@gmail.com>
2024-10-28 22:33:52 +0300

    Initial Commit

---
+++ TrackYourExpenses/TrackYourExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackYourExpensesApp.swift
+//  TrackYourExpenses
+//
+//  Created by mac on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackYourExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackYourExpensesTests/TrackYourExpensesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackYourExpensesTests.swift
+//  TrackYourExpensesTests
+//
+//  Created by mac on 28/10/2024.
+//
+
+import Testing
+@testable import TrackYourExpenses
+
+struct TrackYourExpensesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
